fbd0a7714c29fd345aed6614472a8e5c5e6c4f20
QuanTran <hoangquan1991@gmail.com>
2016-03-28 21:22:27 +0700

    Initial commit

---
+++ app/src/main/java/tran/quan/videostreamer/MainActivity.java
@@ -0,0 +1,132 @@
+package tran.quan.videostreamer;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener,  SurfaceHolder.Callback {
+
+    final static String USERNAME = "admin";
+    final static String PASSWORD = "camera";
+    final static String RTSP_URL = "http://192.168.1.4:8081";
+    private MediaPlayer _mediaPlayer;
+    private SurfaceHolder _surfaceHolder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.setBackgroundDrawableResource(android.R.color.black);
+        SurfaceView surfaceView =
+                (SurfaceView) findViewById(R.id.surfaceView);
+        _surfaceHolder = surfaceView.getHolder();
+        _surfaceHolder.addCallback(this);
+        _surfaceHolder.setFixedSize(320, 240);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        _mediaPlayer = new MediaPlayer();
+        _mediaPlayer.setDisplay(_surfaceHolder);
+
+        Context context = getApplicationContext();
+        Map<String, String> headers = getRtspHeaders();
+        Uri source = Uri.parse(RTSP_URL);
+
+        try {
+            // Specify the IP camera's URL and auth headers.
+            _mediaPlayer.setDataSource(context, source, headers);
+
+            // Begin the process of setting up a video stream.
+            _mediaPlayer.setOnPreparedListener(this);
+            _mediaPlayer.prepareAsync();
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        _mediaPlayer.release();
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        _mediaPlayer.start();
+    }
+
+    private Map<String, String> getRtspHeaders() {
+        Map<String, String> headers = new HashMap<String, String>();
+        String basicAuthValue = getBasicAuthValue(USERNAME, PASSWORD);
+        headers.put("Authorization", basicAuthValue);
+        return headers;
+    }
+
+    private String getBasicAuthValue(String usr, String pwd) {
+        String credentials = usr + ":" + pwd;
+        int flags = Base64.URL_SAFE | Base64.NO_WRAP;
+        byte[] bytes = credentials.getBytes();
+        return "Basic " + Base64.encodeToString(bytes, flags);
+    }
+}
