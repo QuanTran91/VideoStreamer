22fa80a7479cf08da6d295e1b2799b16a1026e0b
QuanTran <hoangquan1991@gmail.com>
2016-03-28 22:17:19 +0700

    Use vitamo lib to stream video

--- app/src/main/java/tran/quan/videostreamer/MainActivity.java
+++ app/src/main/java/tran/quan/videostreamer/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Base64;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -16,117 +17,57 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener,  SurfaceHolder.Callback {
+import io.vov.vitamio.LibsChecker;
+import io.vov.vitamio.widget.MediaController;
+import io.vov.vitamio.widget.VideoView;
 
-    final static String USERNAME = "admin";
-    final static String PASSWORD = "camera";
-    final static String RTSP_URL = "http://192.168.1.4:8081";
-    private MediaPlayer _mediaPlayer;
-    private SurfaceHolder _surfaceHolder;
+public class MainActivity extends AppCompatActivity  {
+    private String pathToFileOrUrl= "http://127.0.0.1:8881";
+    private VideoView mVideoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        if (!LibsChecker.checkVitamioLibs(this))
+            return;
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mVideoView = (VideoView) findViewById(R.id.surface_view);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if (pathToFileOrUrl == "") {
+            Toast.makeText(this, "Please set the video path for your media file", Toast.LENGTH_LONG).show();
+            return;
+        } else {
 
+            /*
+             * Alternatively,for streaming media you can use
+             * mVideoView.setVideoURI(Uri.parse(URLstring));
+             */
+            mVideoView.setVideoPath(pathToFileOrUrl);
+            mVideoView.setMediaController(new MediaController(this));
+            mVideoView.requestFocus();
 
-        Window window = getWindow();
-        window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        window.setBackgroundDrawableResource(android.R.color.black);
-        SurfaceView surfaceView =
-                (SurfaceView) findViewById(R.id.surfaceView);
-        _surfaceHolder = surfaceView.getHolder();
-        _surfaceHolder.addCallback(this);
-        _surfaceHolder.setFixedSize(320, 240);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+//            mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//                @Override
+//                public void onPrepared(MediaPlayer mediaPlayer) {
+//                    // optional need Vitamio 4.0
+//                    mediaPlayer.setPlaybackSpeed(1.0f);
+//                }
+//            });
         }
 
-        return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        _mediaPlayer = new MediaPlayer();
-        _mediaPlayer.setDisplay(_surfaceHolder);
-
-        Context context = getApplicationContext();
-        Map<String, String> headers = getRtspHeaders();
-        Uri source = Uri.parse(RTSP_URL);
-
-        try {
-            // Specify the IP camera's URL and auth headers.
-            _mediaPlayer.setDataSource(context, source, headers);
-
-            // Begin the process of setting up a video stream.
-            _mediaPlayer.setOnPreparedListener(this);
-            _mediaPlayer.prepareAsync();
-        }
-        catch (Exception e) {
-
+    public void startPlay(View view) {
+        if (!TextUtils.isEmpty(pathToFileOrUrl)) {
+            mVideoView.setVideoPath(pathToFileOrUrl);
         }
     }
 
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        _mediaPlayer.release();
-    }
-
-    @Override
-    public void onPrepared(MediaPlayer mp) {
-        _mediaPlayer.start();
-    }
-
-    private Map<String, String> getRtspHeaders() {
-        Map<String, String> headers = new HashMap<String, String>();
-        String basicAuthValue = getBasicAuthValue(USERNAME, PASSWORD);
-        headers.put("Authorization", basicAuthValue);
-        return headers;
-    }
-
-    private String getBasicAuthValue(String usr, String pwd) {
-        String credentials = usr + ":" + pwd;
-        int flags = Base64.URL_SAFE | Base64.NO_WRAP;
-        byte[] bytes = credentials.getBytes();
-        return "Basic " + Base64.encodeToString(bytes, flags);
+    public void openVideo(View View) {
+        mVideoView.setVideoPath(pathToFileOrUrl);
     }
 }
