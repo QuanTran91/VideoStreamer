831da656a9faf5734b97414bc96c70ea03476d8d
QuanTran <hoangquan1991@gmail.com>
2016-05-04 20:11:13 +0700

    Add missing file

---
+++ app/src/main/java/tran/quan/videostreamer/activity/VideoStreamActivity.java
@@ -0,0 +1,13 @@
+package tran.quan.videostreamer.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoStreamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_stream);
+    }
+}
